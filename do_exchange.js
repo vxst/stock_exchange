b2ca75139b098779d5ede492cbfa37d7012f20af
Shan Ting <vxst@vxst.org>
2016-06-26 17:47:52 +0800

    Do exchange finished

---
+++ do_exchange.js
@@ -0,0 +1,246 @@
+var database = require("./db");
+var async = require("async");
+
+function user_money_add(connection, user_id, money, callback){
+	if(Math.abs(money) < 0.01){
+		callback(null);
+		return;
+	}
+	connection.query(
+		"UPDATE user_money SET money=money+? WHERE stock_account_id=?",
+		[money, user_id],
+		(error, result)=>{
+			callback(error);
+		}
+	);
+}
+
+function user_stock_add(connection, user_id, stock_id, amount, callback){
+	async.waterfall([
+		(callback)=>{
+			connection.query(
+				`SELECT COUNT(*) AS count
+					FROM stock_holding
+					WHERE user_id=? AND stock_id=?`,
+				[user_id, stock_id],
+				(error, result)=>{
+					callback(error, result[0].count);
+				});
+		},
+		(current_count, callback)=>{
+			if(current_count == 1){
+				connection.query(
+					`UPDATE stock_holding
+						SET amount = amount+?
+						WHERE stock_id = ? AND user_id=?`,
+					[amount, stock_id, user_id],
+					(error, result)=>{
+						callback(error);
+					}
+				);
+			}else if(current_count == 0){
+				connection.query(
+					`INSERT INTO stock_holding
+						(user_id, stock_id, amount)
+						VALUES(?, ?, ?)`,
+					[user_id, stock_id, amount],
+					(error, result)=>{
+						callback(error);
+					}
+				);
+			}
+		}
+	], (error)=>{
+		if(error){
+			console.log("User stock add error" + error);
+			console.trace();
+		}
+		callback(null);
+	});
+}
+
+function add_history_order(connection, sell_id, buy_id, stock_id, price, amount, callback){
+	connection.query(`INSERT INTO history_orders
+		(sell_user_id, buy_user_id, stock_id, price, amount)
+		VALUES(?, ?, ?, ?, ?)`,
+		[sell_id, buy_id, stock_id, price, amount],
+		(error, result)=>{
+			callback(error);
+		}
+	);
+}
+
+function do_order(connection, stock_id, buy_in_item, sell_out_item, callback){
+	let make_price = (buy_in_item.price + sell_out_item.price) / 2.0;
+	let make_amout = Math.min(buy_in_item.amount, sell_out_item.amount);
+	let buyer_discount = (buy_in_item.price - make_price) * make_amout;
+	async.waterfall([
+		(callback)=>{
+			user_money_add(connection,
+				buy_in_item.user_id, buyer_discount, callback);
+		},
+		(callback)=>{
+			user_money_add(connection,
+				sell_out_item.user_id, make_amout * make_price, callback);
+		},
+		(callback)=>{
+			user_stock_add(connection,
+				buy_in_item.user_id, stock_id, make_amout, callback);
+		},
+		(callback)=>{
+			user_stock_add(connection,
+				sell_out_item.user_id, stock_id, -make_amout, callback);
+		},
+		(callback)=>{
+			add_history_order(connection,
+				sell_out_item.user_id, buy_in_item.user_id,
+				stock_id, make_price, make_amout, callback);
+		}
+	],
+	(error)=>{
+		buy_in_item.amount -= make_amout;
+		sell_out_item.amount -= make_amout;
+		callback(error);
+	});
+}
+
+function do_single_exchange(stock_id, connection, callback){
+	async.waterfall([
+		(callback)=>{
+			connection.query(
+				`LOCK TABLE active_orders WRITE`,
+				(error, result)=>{
+					callback(error);
+				}
+			);
+		},
+		(callback)=>{
+			connection.query(
+				`SELECT id, user_id, price, amount, direction, order_time
+					FROM active_orders
+					WHERE stock_id=? AND direction=TRUE
+					ORDER BY price DESC, order_time ASC`, [stock_id], callback);
+		},
+		(buy_in_list, callback)=>{
+			connection.query(
+				`SELECT id, user_id, price, amount, direction, order_time
+					FROM active_orders
+					WHERE stock_id=? AND direction=FALSE
+					ORDER BY price ASC, order_time ASC`,
+				[stock_id],
+				(error, sell_out_list)=>{
+					callback(null, buy_in_list, sell_out_list);
+				});
+		},
+		(buy_in_list, sell_out_list, callback)=>{
+			connection.query(
+				`DELETE FROM active_orders WHERE stock_id=?`,
+				[stock_id],
+				(error, result)=>{
+					callback(error, buy_in_list, sell_out_list);
+				});
+		},
+		(buy_in_list, sell_out_list, callback)=>{
+			connection.query(
+				`UNLOCK TABLES`,
+				(error, result)=>{
+					callback(error, buy_in_list, sell_out_list);
+				});
+		},
+		(buy_in_list, sell_out_list, callback)=>{
+			async.whilst(
+				()=>{
+					if(buy_in_list.length == 0 || sell_out_list.length == 0)
+						return false;
+					return buy_in_list[0].price > sell_out_list[0].price;
+				},
+				(callback)=>{
+					do_order(connection, stock_id, buy_in_list[0], sell_out_list[0], 
+						(error)=>{
+							if(buy_in_list[0].amount == 0)
+								buy_in_list.splice(0, 1);
+							if(sell_out_list[0].amount == 0)
+								sell_out_list.splice(0, 1);
+							callback(error);
+						});
+				},
+				(error)=>{
+					if(error){
+						console.trace();
+						callback(error);
+					}else{
+						callback(null, buy_in_list, sell_out_list);
+					}
+				}
+			);
+		},
+		(buy_in_list, sell_out_list, callback)=>{
+			full_list = buy_in_list.concat(sell_out_list);
+			async.eachSeries(
+				full_list,
+				(stock_item, callback)=>{
+					connection.query(
+						`INSERT INTO active_orders
+							(id, user_id, stock_id, direction,
+							 price, amount, order_time)
+							VALUES(?, ?, ?, ?, ?, ?, ?)`,
+						[stock_item.id, stock_item.user_id, stock_id,
+						stock_item.direction, stock_item.price, stock_item.amount,
+						stock_item.order_time],
+						(error, result)=>{
+							callback(error);
+						}
+					);
+				},
+				(error)=>{
+					callback(error);
+				}
+			);
+		}
+	],
+	(error)=>{
+		if(error){
+			console.log("Single ERROR" + error);
+		}
+		callback(error);
+	});
+}
+
+function do_exchange(){
+	async.waterfall([
+		(callback)=>{
+			database.get_connection(callback);
+		},
+		(connection, callback)=>{
+			connection.query(
+				"SELECT DISTINCT stock_id FROM active_orders",
+				(error, result)=>{
+					if(error){
+						connection.release();
+						callback("Failed SQL");
+					}else{
+						callback(null, result, connection);
+					}
+				}
+			);
+		},
+		(result, connection, callback)=>{
+			async.eachSeries(
+				result,
+				(stock_id_item, callback)=>{
+					do_single_exchange(stock_id_item.stock_id, connection, callback);
+				},
+				()=>{
+					connection.release();
+					callback(null);
+				}
+			);
+		}
+	],
+	(error)=>{
+		if(error)
+			console.log(error);
+	});
+}
+
+do_exchange();
---
+++ initdb.js
@@ -0,0 +1,121 @@
+var database = require("./db");
+var async = require("async");
+
+function init_database(){
+	database.get_connection(
+		function(err, connection){
+			async.series([
+				function(callback){
+					connection.query(`CREATE TABLE IF NOT EXISTS
+						user(
+							id INT PRIMARY KEY AUTO_INCREMENT,
+							username VARCHAR(32),
+							name VARCHAR(32),
+							password CHAR(20),
+							create_time TIMESTAMP DEFAULT NOW(),
+							national_id VARCHAR(32),
+							address VARCHAR(255),
+							work VARCHAR(32),
+							education VARCHAR(32),
+							work_phone VARCHAR(32),
+							is_admin BOOLEAN,
+							UNIQUE INDEX(username)
+						)`, callback);
+				},
+				function(callback){
+					connection.query(`CREATE TABLE IF NOT EXISTS
+						user_money(
+							id INT PRIMARY KEY AUTO_INCREMENT,
+							stock_account_id INT,
+							password CHAR(20),
+							money DOUBLE,
+							UNIQUE INDEX(stock_account_id),
+							FOREIGN KEY(stock_account_id)
+								REFERENCES user(id)
+								ON DELETE CASCADE
+						)`, callback);
+				},
+				function(callback){
+					connection.query(`CREATE TABLE IF NOT EXISTS
+						stock(
+							id INT PRIMARY KEY AUTO_INCREMENT,
+							name VARCHAR(32),
+							base_price DOUBLE,
+							max_change DOUBLE,
+							next_max_change DOUBLE,
+							active BOOLEAN,
+							INDEX(id, active)
+						)`, callback);
+				},
+				function(callback){
+					connection.query(`CREATE TABLE IF NOT EXISTS
+						active_orders(
+							id INT PRIMARY KEY AUTO_INCREMENT,
+							user_id INT,
+							stock_id INT,
+							direction BOOLEAN,
+							price DOUBLE,
+							amount INT,
+							order_time TIMESTAMP DEFAULT NOW(),
+							INDEX(user_id, stock_id),
+							INDEX(direction),
+							INDEX(price),
+							FOREIGN KEY(user_id)
+								REFERENCES user(id)
+								ON DELETE CASCADE,
+							FOREIGN KEY(stock_id)
+								REFERENCES stock(id)
+								ON DELETE CASCADE
+						)`, callback);
+				},
+				function(callback){
+					connection.query(`CREATE TABLE IF NOT EXISTS
+						history_orders(
+							id INT PRIMARY KEY AUTO_INCREMENT,
+							stock_id INT,
+							sell_user_id INT,
+							buy_user_id INT,
+							price DOUBLE,
+							amount INT,
+							complete_time TIMESTAMP DEFAULT NOW(),
+							INDEX(stock_id),
+							INDEX(sell_user_id),
+							INDEX(buy_user_id),
+							FOREIGN KEY(stock_id)
+								REFERENCES stock(id)
+								ON DELETE CASCADE,
+							FOREIGN KEY(sell_user_id),
+								REFERENCES user(id)
+								ON DELETE SET NULL,
+							FOREIGN KEY(buy_user_id),
+								REFERENCES user(id)
+								ON DELETE SET NULL
+						)`, callback);
+				},
+				function(callback){
+					connection.query(`CREATE TABLE IF NOT EXISTS
+						stock_holding(
+							id INT PRIMARY KEY AUTO_INCREMENT,
+							user_id INT,
+							stock_id INT,
+							amount INT,
+							INDEX(user_id),
+							INDEX(stock_id),
+							FOREIGN KEY(user_id)
+								REFERENCES user(id)
+								ON DELETE CASCADE,
+							FOREIGN KEY(stock_id)
+								REFERENCES stock(id)
+								ON DELETE CASCADE
+						)`, callback);
+				},
+				function(callback){
+					connection.release();
+					callback(null);
+				}
+			]);
+		}
+	);
+}
+
+init_database();
